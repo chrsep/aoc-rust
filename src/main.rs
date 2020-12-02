47b8c87d00a800dbeb2aca67bf833ee2fc5f5c4e
Chrisando Pramudhita <chrsep@protonmail.com>
2020-12-02 17:08:46 +0700

    learn how to use iter.

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,9 @@ fn main() {
 }
 
 fn day_one(input: String) {
-    println!("{}", input);
+    let values = input.split("\n");
+    let numbers  = values.map(|value| {
+        return value.parse::<i32>().unwrap();
+    }).collect::<Vec<i32>>();
+    println!("{:?}", numbers)
 }
